1d4a0fc3c8ed735cdd9db7df79c66761556c025a
emkael <emkael@tlen.pl>
2018-10-13 16:24:18 +0200

    S3 client for file upload

---
+++ kurier/S3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.IO;
+using System.Windows.Forms;
+using Amazon;
+
+namespace kurier
+{
+    class S3
+    {
+        private AmazonS3Client _client;
+
+        public S3()
+        {
+            this._client = new AmazonS3Client(Properties.S3Settings.Default.AWS_ACCESS_KEY, Properties.S3Settings.Default.AWS_SECRET_KEY, RegionEndpoint.EUCentral1);
+        }
+
+        public void send(string bucket, string filePath, string remotePath)
+        {
+            PutObjectRequest request = new PutObjectRequest { 
+                BucketName = bucket,
+                Key = remotePath,
+                ContentBody = File.ReadAllText(filePath) };
+            this._client.PutObject(request);
+        }
+    }
+}
