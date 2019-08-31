8a3fa1426d45ec8baa557f78d3764d3966b0e1fe
emkael <emkael@tlen.pl>
2019-08-31 17:54:34 +0200

    Ensuring only one app instance is running

--- spedytor/Program.cs
+++ spedytor/Program.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace spedytor
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(false, "Spedytor - emkael.info");
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            if (!mutex.WaitOne(TimeSpan.FromSeconds(1), false))
+            {
+                MessageBox.Show("Spedytor już działa! Może gdzieś w tle?", "Błąd!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
         }
     }
 }
